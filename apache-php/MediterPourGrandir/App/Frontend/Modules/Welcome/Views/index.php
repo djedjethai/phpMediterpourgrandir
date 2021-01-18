e9f4f38c9452f3f976aeda241aa8edc44364657c
djedjethai <djedje.thai.ok@gmail.com>
2021-01-19 05:36:54 +0700

    index ok

--- apache-php/MediterPourGrandir/App/Frontend/Modules/Welcome/Views/index.php
+++ apache-php/MediterPourGrandir/App/Frontend/Modules/Welcome/Views/index.php
@@ -20,14 +20,14 @@
     </div>
     <div class="textwelcome">
     	<div class="paragraph">
-    		<p>Vivant en Thaïlande depuis plus de 15 ans, j’ai eu la chance suivre des formations de méditation sérieuses. j’ai notamment obtenue la qualification d’Instructeur au sein de l’institut <a href=http://www.samathi.com/2016/institute-detail.php?actid=1>willpower institut”</a>. Méditer quotidiennement nous aide à prendre du recule par rapport aux évènements du quotidien et par rapport à nous-même, progressivement nos prises de conscience s’éclaircissent, d’où ‘’méditation de pleine conscience’’.<br /> 
-    		De plus, affermissant la force de volonté, nous développons nos aptitudes à rectifier nos comportements, nos paroles, et finalement nos pensées, ainsi nous optimisons notre bien-être. Dans cet enseignement je n’invente rien, je ne fais que rapporter une technique de méditation traditionnelle résultante de l’expérience d’expert, en l’occurrence de <a href=https://en.wikipedia.org/wiki/Mun_Bhuridatta>Luang Pu Mun Bhuridatta</a>.</p>
+    		<p><span class="u-margin-left-medium">Vivant</span> en Thaïlande depuis plus de 15 ans, j’ai eu la chance suivre des formations de méditation sérieuses. j’ai notamment obtenue la qualification d’Instructeur au sein de l’institut <a href=http://www.samathi.com/2016/institute-detail.php?actid=1>willpower institut”</a>. Méditer quotidiennement nous aide à prendre du recule par rapport aux évènements du quotidien et par rapport à nous-même, progressivement nos prises de conscience s’éclaircissent, d’où ‘’méditation de pleine conscience’’.</p>
+    		<p><span class="u-margin-left-medium">De</span> plus, affermissant la force de volonté, nous développons nos aptitudes à rectifier nos comportements, nos paroles, et finalement nos pensées, ainsi nous optimisons notre bien-être. Dans cet enseignement je n’invente rien, je ne fais que rapporter une technique de méditation traditionnelle résultante de l’expérience d’expert, en l’occurrence de <a href=https://en.wikipedia.org/wiki/Mun_Bhuridatta>Luang Pu Mun Bhuridatta</a>.</p>
     	</div> 
     </div>
 </section>
 
 <section class="section-about">
-	<div class="u-center-text u-margin-bottom-big">
+	<div class="u-center-text u-margin-bottom-verybig">
                 <h2 class="heading-secondary">
                     Les bénéfices de la méditation
                 </h2>
@@ -55,7 +55,6 @@
                         </div>
 		</div>
 	</div>
-<hr class="u-separation-section" />
 </section>
 
 <section class="section-explication">
@@ -66,11 +65,28 @@
     </div>
     <div class="textwelcome">
     	<div class="paragraph">
-    		<p>La méditation consiste à rester concentré sur un support tel la respiration. L’exercice n’est pas si simple car l’esprit ne cesse de vagabonder, de rêvasser, de ressasser, aussi nous devons continuellement nous reconcentrer. De la concentration découle l’attention, proportionnellement à notre niveau nos qualités (de concentration et d’attention) s’améliorent.</p>
+    		<p><span class="u-margin-left-medium">La</span> méditation consiste à rester concentré sur un support tel la respiration. L’exercice n’est pas si simple car l’esprit ne cesse de vagabonder, de rêvasser, de ressasser, aussi nous devons continuellement nous reconcentrer. De la concentration découle l’attention, proportionnellement à notre niveau nos qualités (de concentration et d’attention) s’améliorent.</p>
 
-			<p>Affermir notre potentiel de concentration et d’attention optimise nos performances dans tous les domaines car, quelle que soit la tâche, intellectuellement nous devenons plus endurant et faisons moins d’erreurs. Rappeler la concentration nous entraine à moins dépendre de nos humeurs-émotions (souvent appelé ‘’le lâcher prise’’ en Français) , progressivement un état de bien être particulier s’installe en nous.</p>
+			<p><span class="u-margin-left-medium">Affermir</span> notre potentiel de concentration et d’attention optimise nos performances dans tous les domaines car, quelle que soit la tâche, intellectuellement nous devenons plus endurant et faisons moins d’erreurs. Rappeler la concentration nous entraine à moins dépendre de nos humeurs-émotions (souvent appelé ‘’le lâcher prise’’ en Français) , progressivement un état de bien être particulier s’installe en nous.</p>
 
-			<p>Notre pratique méditative ainsi que nos efforts personnels à entretenir au mieux notre calme intérieur au quotidien optimise nos prises de conscience (notons qu’ à maturité nous saurons constater les mécanismes de notre esprit), et donc modifie nos convictions. Nous devenons plus raisonnables, plus réfléchit, nous grandissons.</p>
+			<p><span class="u-margin-left-medium">Notre</span> pratique méditative ainsi que nos efforts personnels à entretenir au mieux notre calme intérieur au quotidien optimise nos prises de conscience (notons qu’ à maturité nous saurons constater les mécanismes de notre esprit), et donc modifie nos convictions. Nous devenons plus raisonnables, plus réfléchit, nous grandissons.</p>
+    	</div> 
+    </div>
+</section>
+
+<section class="section-formation">
+	<div class="u-center-text u-margin-bottom-big">
+            <h2 class="heading-secondary">
+                la formation
+            </h2>
+    </div>
+    <div class="textwelcome">
+    	<div class="paragraph">
+    		<p><span class="u-margin-left-medium">Cet</span> enseignement est gratuit. Il se compose de 46 leçons reparties en  5 chapitres. Tous les aspects de la méditation sont expliqués : une technique extrêmement efficace, les obstacles à éviter, les chan (djana) et les yan (insight). Il s’adresse à tous les niveaux car il part de zéro et décrit toutes les étapes. Aussi à l’issue la méditation de pleine conscience n’aura plus de secret pour vous.</p>
+
+			<p><span class="u-margin-left-medium">Après</span> avoir ouvert votre compte, vous avez accès à une leçon tous les 4 jours. Cette contrainte n’est pas anodine, afin de développer la méditation if faut la pratiquer quotidiennement durant un certain temps avant d’en constater les bénéfices. Ainsi cet enseignement vous incite, pour commencer, à méditer durant 6 mois. Idéalement cela ne sera qu’un début, car à l’issue, afin d’entretenir vos acquis et continuer à progresser, vous devrez poursuivre.</p>
+ 
+			<p><span class="u-margin-left-medium">Un</span> forum est mis à disposition afin de répondre aux questions, si vous avez des doutes, ou ne comprenez pas certaines explications. Ce dernier vous permet également d’échanger avec d’autres pratiquants sur des sujets lies à la méditation.</p>
     	</div> 
     </div>
 </section>
