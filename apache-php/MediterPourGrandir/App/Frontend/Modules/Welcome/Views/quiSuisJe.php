01c8b2beb0280bdd3e188c6f4fb99cc96bf06819
djedjethai <djedje.thai.ok@gmail.com>
2021-01-23 19:28:54 +0700

    qui suis je ok

--- apache-php/MediterPourGrandir/App/Frontend/Modules/Welcome/Views/quiSuisJe.php
+++ apache-php/MediterPourGrandir/App/Frontend/Modules/Welcome/Views/quiSuisJe.php
@@ -1,17 +1,21 @@
 <main>
 	<section class="section-quisuisje">
-	<div class="paragraph">
-		<!--<div class="col-1-of-2">-->
-                   	<div class="showpicture">
-			<img srcset="Web/images/welcome/wel-1-medium.jpg 300w, Web/images/welcome/wel-1-medium.jpg 1000w" size="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px" alt="Photo 1" class="showpicture__photo" src="Web/images/welcome/wel-1-medium.jpg">
+		<div class="showpicture u-margin-bottom-big">
+			<img srcset="../Web/images/welcome/wel-1-medium.jpg 300w, ../Web/images/welcome/wel-1-medium.jpg 1000w" size="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px" alt="Photo 1" class="showpicture__photo" src="../Web/images/welcome/wel-1-medium.jpg">
+		</div>
+		<div class="textwelcome u-center-text">
+			<div class="paragraph">
+				<p>Instructeur de méditation<br/>
+				(Willpower Institute Bangkok)</p>
+				<p class="u-margin-bottom-medium">Instructeur de haute méditation<br/>
+				(Willpower Institute Bangkok)</p>	
+		
+		<p>pratiquant de meditaion depuis de nombreuses annee, cette technique m’a permis de changer, de devenir plus raisonnable, compatissant, respectueux, tolerant, progresivement ma qualite de vie s’est ameliorer. Finalement, et par chance, j’ai eu l’oppotunite d’approndir mes connaisssances de la meditation en suivant des formations. Elles m’ont permis de mieux comprendre les tenants et aboutisants de cette technique, et donc de pratiquer plus effiacement. 
+<br />
+Je continue de pratique qotidiennement, et aime occasionelement faire des retraites dans les foret aux nord de la Thailand.</p>
+
 			</div>
-		<!--</div>-->
-		<p class="u-center-text"><img src="picprofilre.jpg" class="pro" alt="image" /></p>
-		<p class="u-center-text">Instructeur de méditation<br/>
-			(Willpower Institute Bangkok)</p>
-		<p class="u-center-text">Instructeur de haute méditation<br/>
-			(Willpower Institute Bangkok)</p>
-	</div>
+		</div>
 	</section>
 </main>
 
