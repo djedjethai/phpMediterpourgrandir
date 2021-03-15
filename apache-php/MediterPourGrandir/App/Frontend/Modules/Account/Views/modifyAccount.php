237f46b246e5633d78488a48ad4cc5301e2d3db8
djedjethai <djedje.thai.ok@gmail.com>
2021-03-16 04:43:33 +0700

    css section account done

--- apache-php/MediterPourGrandir/App/Frontend/Modules/Account/Views/modifyAccount.php
+++ apache-php/MediterPourGrandir/App/Frontend/Modules/Account/Views/modifyAccount.php
@@ -1,36 +1,42 @@
      	<div class="rowgrid">
-             	<div class="booknopic">
-<h2>Modifier votre compte</h2>
+             	<div class="bookaccount">
+			<div class="u-center-text u-margin-top-medium u-margin-bottom-medium">
+              			<h2 class="heading-secondary">
+                                    	Modifiez votre compte
+                                </h2>
+                        </div>
 
 
 
-<?php if($errMessage) {
-	if($errMessage === 'pseudoRepeat') { ?>
-	<p class="text-danger">Ce pseudo existe deja.</p>
-<?php } elseif($errMessage === 'emailRepeat') { ?>
-	<p class="text-danger">Cet e-mail existe deja.</p>
-<?php } elseif($errMessage === 'passwordIncorrect') { ?>
-	<p class="text-danger">Le mot de passe est incorrect.</p>
-<?php } elseif($errImage) { ?>
-	<p class="text-danger"><?=$errImage?></p>
-<?php }} ?>
-
-
-<form action="" method="post" enctype="multipart/form-data">
-	<p>
-
-    <?= $form ?>
-    
-    <?php if ($student->picture()) { ?>
-	  	<input type="checkbox" id="deletePic" name="deletePic">
-	  	<label for="deletePic">Effacer la photo</label><br />
-	 <?php } ?>
-    
-    <input type="submit" value="Modifier" />
-  </p>
-</form>
-
-<a href="/Account/modifyPassword.php">Modifier votre mot de passe</a>
+			<?php if($errMessage) {
+				if($errMessage === 'pseudoRepeat') { ?>
+				<p class="text-danger">Ce pseudo existe deja.</p>
+			<?php } elseif($errMessage === 'emailRepeat') { ?>
+				<p class="text-danger">Cet e-mail existe deja.</p>
+			<?php } elseif($errMessage === 'passwordIncorrect') { ?>
+				<p class="text-danger">Le mot de passe est incorrect.</p>
+			<?php } elseif($errImage) { ?>
+				<p class="text-danger"><?=$errImage?></p>
+			<?php }} ?>
+			
+			
+			<form action="" method="post" enctype="multipart/form-data">
+				<p>
+			
+			    <?= $form ?>
+			    
+			    <?php if ($student->picture()) { ?>
+			    	<div class="paragraph">
+				  	<input type="checkbox" id="deletePic" name="deletePic">
+				  	<label for="deletePic">Effacer la photo</label><br />
+				</div>	 
+			<?php } ?>
+			    
+			    <input type="submit" class="btn-choice-btn u-margin-bottom-small" value=" Modifier" />
+			  </p>
+			</form>
+			
+			<a href="/Account/modifyPassword.php">Modifier votre mot de passe</a>
 		</div>
 	</div>
 </section>
--- apache-php/MediterPourGrandir/App/Frontend/Modules/Account/Views/modifyPassword.php
+++ apache-php/MediterPourGrandir/App/Frontend/Modules/Account/Views/modifyPassword.php
@@ -1,21 +1,36 @@
-<p><a href="/Account/modifyAccount.php">Retour &larr;</a></p>
+	<div class="rowgrid">
+             	<div class="bookaccount">
+			<div class="u-center-text u-margin-top-medium u-margin-bottom-medium">
+              			<h2 class="heading-secondary">
+                                    	Modifiez votre mot de passe
+                                </h2>
+                        </div>
 
-<h2>Modifier votre mot de passe</h2>
+			<div class="u-wrap-goback">
+				<a href="/Account/modifyAccount.php" class="btn-text">&Larr;Retour</a>
+			</div>
 
-<?php if($errMessage) {
-	if ($errMessage === 'passwordIncorrect') { ?>
-	<p class="text-danger">Le mot de passe est incorrect.</p>
-	<?php } 
-	else if($errMessage === 'confirmNewPasswordIncorrect') { ?>
-	<p class="text-danger">La confirmation de votre mot de passe ne correspond pas.</p>
-<?php }} ?>
+			<?php if($errMessage) {
+				if ($errMessage === 'passwordIncorrect') { ?>
+				<p class="text-danger">Le mot de passe est incorrect.</p>
+				<?php } 
+				else if($errMessage === 'confirmNewPasswordIncorrect') { ?>
+				<p class="text-danger">La confirmation de votre mot de passe ne correspond pas.</p>
+			<?php }} ?>
+			
+			
+			<form action="" method="post">
+				<p>
+			
+			    <?= $form ?>
 
 
-<form action="" method="post">
-	<p>
+			<input type="submit" class="btn-choice-btn u-margin-bottom-small" value=" Modifier" />
+			  </p>
+			</form>
+		</div>
+	</div>
+</section>
 
-    <?= $form ?>
-    
-    <input type="submit" value="Modifier" />
-  </p>
-</form>
+
+
