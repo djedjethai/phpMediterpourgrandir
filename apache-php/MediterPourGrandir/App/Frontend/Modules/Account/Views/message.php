aa42812ce653e034ec6187572929e4e0ad9c5b18
djedjethai <djedje.thai.ok@gmail.com>
2021-03-18 05:25:46 +0700

    css table notification works

--- apache-php/MediterPourGrandir/App/Frontend/Modules/Account/Views/message.php
+++ apache-php/MediterPourGrandir/App/Frontend/Modules/Account/Views/message.php
@@ -1,32 +1,32 @@
 
-<div class="rowgrid">
-<div class="table-responsive">
-	<?php if(!empty($messages)) {
-			
-	?>
-	<table class="table table-hover">
-	  	<thead><tr><th>Titre</th><th>Date d'ajout</th><th>Consulter</th></tr></thead>
-		<?php
-
-			foreach($messages as $message)
-			{
-				if($message->status()){
-					$iconValid = '<i style="color: green;" class="far fa-check-circle fa-lg"></i>';
-				} else {
-					$iconValid = '<i style="color: black;" class="far fa-check-circle fa-lg"></i>';
-				}
-
-			  echo '<tbody><tr><td>', $message->titleNews(), '</td><td>le ', $message->dateAjout(), '</td><td><a href="/news/news-', $message->idNews(), '.php">', $iconValid, '</a></td></tr></tbody>', "\n";
-			}
+	<div class="rowgrid">
+		<?php if(!empty($messages)) {
+				
 		?>
-	</table>
-	<?php } else { ?>
-
-		<h3>Vous n'avez pas de messages</h3>
-
-	<?php } ?>
-</div>
-</div>
+		<table>
+			<div class="paragraph">
+		  	<thead><tr><th>Titre</th><th>Date d'ajout</th><th>Consulter</th></tr></thead>
+			<?php
+	
+				foreach($messages as $message)
+				{
+					if($message->status()){
+						$iconValid = '<i style="color: green;" class="far fa-check-circle fa-lg"></i>';
+					} else {
+						$iconValid = '<i style="color: black;" class="far fa-check-circle fa-lg"></i>';
+					}
+	
+				  echo '<tbody><tr><td>', $message->titleNews(), '</td><td>le ', $message->dateAjout(), '</td><td><a href="/news/news-', $message->idNews(), '.php">', $iconValid, '</a></td></tr></tbody>', "\n";
+				}
+			?>
+		</table>
+		<?php } else { ?>
+	
+			<h3>Vous n'avez pas de messages</h3>
+	
+		<?php } ?>
+		</div>
+	</div>
 </section>	
 
 
