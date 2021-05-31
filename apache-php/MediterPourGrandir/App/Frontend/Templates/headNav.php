494e08539d3ebaca9fd58f0fcf4a2138d9de2cfa
djedjethai <djedje.thai.ok@gmail.com>
2021-05-31 07:48:01 +0700

    before merge

--- apache-php/MediterPourGrandir/App/Frontend/Templates/headNav.php
+++ apache-php/MediterPourGrandir/App/Frontend/Templates/headNav.php
@@ -2,7 +2,6 @@
 <html lang="fr">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <!--<meta charset="utf-8">-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   
@@ -10,25 +9,19 @@
   <link rel="stylesheet" href="/Web/css/style.css">
   <link rel="stylesheet" href="/css/<?=$module?>/<?=$view?>.css">
   <link rel="stylesheet" href="/Web/css/bootstrap-4.3.1-dist/css/bootstrap.css">
-  <!--<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>-->
   <script src="https://kit.fontawesome.com/d60d1dd85c.js" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>-->
   <script src="/js/<?=$module?>/<?=$view?>.js"></script>	
 
 </head>
 <body>
-  <!--<nav class="navbar navbar-expand-md bg-dark navbar-dark">-->
   <nav class="navbar navbar-expand-md navbar-light">
-  <!-- Brand -->
   
-  <!-- Toggler/collapsibe Button -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <!-- Navbar links -->
   <div class="collapse navbar-collapse" id="collapsibleNavbar">
     <ul class="navbar-nav mr-auto">
 
@@ -71,8 +64,6 @@
 
 			
 			 
-			<!--<li class="dropLi"><a href="/learn/deconnexion.php"><button class="btn btn-sm btn-secondary" type="button">Deconnexion</button></a></li>
-				<a href="#"><button class="btn btn-sm btn-secondary ml-3" type="button"><i style="padding: 0px 4px;" class="fas fa-user-alt fa-lg"></i></button></a>-->
 	</form>
 	<?php } else { ?>
 	<li class="nav-item">
