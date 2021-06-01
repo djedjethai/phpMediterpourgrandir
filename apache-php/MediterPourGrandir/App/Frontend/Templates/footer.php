99c16aecb85a3f3176e4762b4e583aac9d168cc3
djedjethai <djedje.thai.ok@gmail.com>
2021-06-02 05:37:01 +0700

    building the footer

--- apache-php/MediterPourGrandir/App/Frontend/Templates/footer.php
+++ apache-php/MediterPourGrandir/App/Frontend/Templates/footer.php
@@ -1,61 +1,49 @@
-<!-- Footer 
-<footer class="page-footer font-small special-color-dark pt-4">-->
-<footer class="shadow-lg page-footer">  
+<footer class="footer-distributed">
+<div class="rowgrid">
+		<div class="col-1-of-3">
+			<div class="footer-left">
+				<h3>About<span>Eduonix</span></h3>
 
-  <!-- Footer Elements -->
-  <?php if(!$user->isUser()) { ?>
-  <div class="container">
-     <ul class="list-unstyled list-inline text-center py-2">
-    <li class="list-inline-item">
-      <h5 class="paragraph">Apprenez a mediter, c'est GRATUIT !</h5>
-    </li>
-    <li class="list-inline-item">
-      <a href="/auth/signUp.php" class="btn btn-success btn-rounded">S'inscrir !</a>
-    </li>
-  </ul>
-<?php } ?>
+				<p class="footer-links">
+					<a href="#">Home</a>
+					|
+					<a href="#">Blog</a>
+					|
+					<a href="#">About</a>
+					|
+					<a href="#">Contact</a>
+				</p>
 
-    <!-- Social buttons -->
-    <ul class="list-unstyled list-inline text-center">
-      <li class="list-inline-item">
-        <a class="btn-floating btn-fb mx-1">
-          <i class="fab fa-facebook-f"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-tw mx-1">
-          <i class="fab fa-twitter"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-gplus mx-1">
-          <i class="fab fa-google-plus-g"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-li mx-1">
-          <i class="fab fa-linkedin-in"> </i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-dribbble mx-1">
-          <i class="fab fa-dribbble"> </i>
-        </a>
-      </li>
-    </ul>
-    <!-- Social buttons -->
-
-  </div>
-  <!-- Footer Elements -->
-
-  <!-- Copyright -->
-   <div class="footer-copyright text-center py-3">© 2018 Copyright:
-    <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
-  </div>
-  <!-- Copyright -->
-
-</footer>
-</body>
+				<p class="footer-company-name">© 2019 Eduonix Learning Solutions Pvt. Ltd.</p>
+			</div>
+		</div>
+		<div class="col-1-of-3">
+			<div class="footer-center">
+				
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+91 22-27782183</p>
+				</div>
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@eduonix.com">support@eduonix.com</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-1-of-3">
+			<div class="footer-right">
+				<p class="footer-company-about">
+					<span>About the company</span>
+					We offer training and skill building courses across Technology, Design, Management, Science and Humanities.</p>
+				<div class="footer-icons">
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-youtube"></i></a>
+				</div>
+			</div>
+		</div>
+		</footer>
+	</body>
 </html>
-
-<!-- Footer -->
