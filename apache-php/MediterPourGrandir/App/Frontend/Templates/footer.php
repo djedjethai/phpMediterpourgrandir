a91a7d7919b0fdd6b2f89afca09eda1f098e707a
djedjethai <djedje.thai.ok@gmail.com>
2021-06-05 05:34:22 +0700

    footer ok, left the icons

--- apache-php/MediterPourGrandir/App/Frontend/Templates/footer.php
+++ apache-php/MediterPourGrandir/App/Frontend/Templates/footer.php
@@ -5,11 +5,11 @@
 				<h3>Méditer<span> Pour Grandir</span></h3>
 
 				<p class="footer-links">
-					<a href="#">Acceuil</a>
+					<a href="/">Acceuil</a>
 					|
-					<a href="#">Qui suis-je</a>
+					<a href="/welcome/QuiSuisJe.php">Qui suis-je</a>
 					|
-					<a href="#">Contact</a>
+					<a href="/welcome/contact.php">Contact</a>
 				</p>
 				<?php if(!$user->isUser()) { ?>
       					<a href="/auth/signUp.php" class="btn-choice-resize-meddium">Apprendre à méditer</a>
@@ -22,25 +22,21 @@
 				
 				<div>
 					<i class="fa fa-phone"></i>
-					<p>+91 22-27782183</p>
+					<p>+66 881-806900</p>
 				</div>
 				<div>
 					<i class="fa fa-envelope"></i>
-					<p><a href="mailto:support@eduonix.com">support@eduonix.com</a></p>
+					<p>mediterpourgrandir@gmail.com</p>
 				</div>
 			</div>
 		</div>
 		<div class="col-1-of-3">
 			<div class="footer-right">
 				<p class="footer-company-about">
-					<span>About the company</span>
-					We offer training and skill building courses across Technology, Design, Management, Science and Humanities.</p>
+					<span>A notre sujet</span>
+					Cet enseignement n'invente rien, il ne fais que rapporter une technique de méditation traditionnelle résultante de l’expérience d’expert.</p>
 				<div class="footer-icons">
 					<a href="#"><i class="fa fa-facebook"></i></a>
-					<a href="#"><i class="fa fa-twitter"></i></a>
-					<a href="#"><i class="fa fa-instagram"></i></a>
-					<a href="#"><i class="fa fa-linkedin"></i></a>
-					<a href="#"><i class="fa fa-youtube"></i></a>
 				</div>
 			</div>
 		</div>
