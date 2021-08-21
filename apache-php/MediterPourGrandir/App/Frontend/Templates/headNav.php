2295da9502ef8e3861efea216542f1fdebf1bf6d
djedjethai <djedje.thai.ok@gmail.com>
2021-08-21 16:06:20 +0700

    navbar done but still not responsive

--- apache-php/MediterPourGrandir/App/Frontend/Templates/headNav.php
+++ apache-php/MediterPourGrandir/App/Frontend/Templates/headNav.php
@@ -17,17 +17,30 @@
 	<div class="navcontainer">
 	<?php if($user->isUser()){ ?>
 		<div class="signed">
-			<img src="Web/images/navbar/logo.png" alt="the logo" class="logo" />
-			<div class="signed-route">
-	        		<a class="menu-route__item" href="/">Acceuil</a>
-	        		<a class="menu-route__item" href="/welcome/QuiSuisJe.php">Qui suis-je</a>
-	        		<a class="menu-route__item" href="/welcome/contact.php">Contact</a>
+			<div class="unsigned-route">
+				<ul class="dropUl">
+                           	<li class="dropLi">
+			      		<a class="dropA" href="#"><i class="fa fa-list fa-lg"></i></a>
+				
+                              		<ul class="dropdown">
+                                		<li class="dropLi"><a class="dropA" href="/">Acceuil</a></li>
+                                		<li class="dropLi"><a class="dropA" href="/welcome/QuiSuisJe.php">Qui-suis-je</a></li>
+                                		<li class="dropLi"><a class="dropA" href="/welcome/contact.php">Contact</a></li>
+                              		</ul>
+                           	</li>
+                        	</ul>    
+				<div class="unsigned-route__item">
+             				<a class="unsigned-route__link" href="/learn/learn.php">Enseignement</a>
+              			</div>
+                		<div class="unsigned-route__item">
+                        		<a class="unsigned-route__link" href="/news/index-1.php">Forum</a>
+                		</div>
 	      		</div>
 		</div>
 		<div class="signed-form">         
                         <ul class="dropUl">
                            <li class="dropLi">
-                              <a class="dropA" href="#"><i style="margin-left: 100px;" class="fas fa-user-alt fa-lg"></i></a>
+			      <a class="dropA" href="#"><i style="margin-left: 100px;" class="fas fa-user-alt fa-lg"></i></a>
                               <ul class="dropdown">
                                 <li class="dropLi"><a class="dropA" href="/Account/message.php">Message
                                    <?php if($student->message()) {?> 
@@ -42,17 +55,18 @@
                         </ul>               
         	</div>
 	<?php } else { ?>
-		<ul class="unsigned-route">
-        		<li class="unsigned-route__item">
-             			<a class="unsigned-route__link" href="/">Acceuil<span class="sr-only">(current)</span></a>
-              		</li>
-                	<li class="unsigned-route__item">
+		<div class="unsigned-route">
+			<!--<a class="unsigned-route__link" href="/"><img class="unsigned-route__img" src="Web/images/navbar/logo.png" alt="the logo" class="logo" /></a>-->
+        		<div class="unsigned-route__item">
+             			<a class="unsigned-route__link" href="/">Acceuil</a>
+              		</div>
+                	<div class="unsigned-route__item">
                       		<a class="unsigned-route__link" href="/welcome/QuiSuisJe.php">Qui suis-je</a>
-                	</li>
-                	<li class="unsigned-route__item">
+                	</div>
+                	<div class="unsigned-route__item">
                         	<a class="unsigned-route__link" href="/welcome/contact.php">Contact</a>
-                	</li>
-    		</ul>
+                	</div>
+    		</div>
             	<div class="unsigned-form">
                         <a href="/auth/signIn.php"><button class="btn-choice-connecter" type="button">Se connecter</button></a>
                         <a href="/auth/signUp.php"><button class="btn-choice-inscrir" type="button">S'inscrir</button></a>
