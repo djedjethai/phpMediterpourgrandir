3a5c794acb8d63adcfee43a470b5d7b4a86df8d0
djedjethai <djedje.thai.ok@gmail.com>
2021-10-21 05:35:23 +0700

    instructeur section, css is doing better left align image to right and add responsive behaviour

--- apache-php/MediterPourGrandir/App/Frontend/Modules/Welcome/Views/quiSuisJe.php
+++ apache-php/MediterPourGrandir/App/Frontend/Modules/Welcome/Views/quiSuisJe.php
@@ -1,9 +1,9 @@
 <main>
 	<section class="section-quisuisje">
-		<div class=textwelcome>
-			<div class="textwelcome_info">
-			<div class="paragraph">
-			<ul>
+		<div class=textinstructeur>
+			<div class="textinstructeur_info">
+				<div class="paragraph">
+				<ul>
 				<li><span class=info>Nom:</span>Bidault</li>
 				<li><span class=info>Prenom:</span>Jerome</li>
 				<li><span class=info>Profession:</span>Web Developper</li>
@@ -18,21 +18,22 @@
 						<li>Maitrise de bouddhisme<br />
 				Delivré par: <a href="https://www.onab.go.th/th/page/item/index/id/10">Office National Tailandais du Buddisme</a></li>	
 					</ul>
+				</div>
 			</div>
-			</div>
-			<div class=textwelcome_picture>
-				<div class="showpicture">
-				<img srcset="../Web/images/welcome/djedje.jpg 300w, ../Web/images/welcome/djedje.jpg 1000w" size="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px" alt="Photo 1" class="showpicture__photo" src="../Web/images/welcome/djedje.jpg">
-			</div>
+			<div class=textinstructeur_picture>
+				<div class="showpictureinstructeur">
+				<img srcset="../Web/images/welcome/djedje.jpg 300w, ../Web/images/welcome/djedje.jpg 1000w" size="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px" alt="Photo 1" class="showpictureinstructeur__photo" src="../Web/images/welcome/djedje.jpg">
+				</div>
 			</div>
 		</div>
 		<div class=instructor-note>
 
 		<h3>Note de l'instructeur:</h3>
-		<p>Pratiquant de méditation depuis plus de 20 ans, j’ai eu l'opportunité d’approfondir mes connaissances (de la méditation) en suivant diverses formations. S'instruire permet de mieux comprendre les tenants et les aboutissants de cette discipline et donc de la pratiquer plus efficacement. La technique enseigné ici résulte de l'expérience de plusieurs générations d'expert ainsi elle s'avère particulièrement efficace. Je suis heureux de pouvoir rapporter ce savoir afin d’offrir à d’autres l’opportunité de le découvrir.<p/>
-			
-		<p>Méditer affermit la concentration-attention, l'utiliser à moins dépendre de nous humeurs-émotions nous aide à "lâcher prise", nous libère de certains mécanismes intérieurs indépendant de notre volonté, ainsi nous appréhendons les différentes situations du quotidien avec plus de recule, nous devenons plus claire-voyant par consequent nous prenons de meilleurs décisions. Progressivement nous allons changer, devenir plus raisonnable, compatissant, respectueux, tolérant, ouvert d'esprit, notre bien-être va s'affermir</p>		
-		<p>La méditation de pleine conscience est une médiation laïque aussi elle s’adresse à tous. Si vous êtes croyant elle vous aidera à pratiquer votre religion.</p>
+		<p><span class="paragraphe">Pratiquant<span> de méditation depuis plus de 20 ans, j’ai eu l'opportunité d’approfondir mes connaissances (de la méditation) en suivant diverses formations. S'instruire permet de mieux comprendre les tenants et les aboutissants de cette discipline et donc de la pratiquer plus efficacement. La technique enseigné ici résulte de l'expérience de plusieurs générations d'expert ainsi elle s'avère particulièrement efficace. Je suis heureux de pouvoir rapporter ce savoir afin d’offrir à d’autres l’opportunité de le découvrir.<p/>
+		<br />	
+		<p><span class="paragraphe">Méditer</span> affermit la concentration-attention, l'utiliser à moins dépendre de nous humeurs-émotions nous aide à "lâcher prise", nous libère de certains mécanismes intérieurs indépendant de notre volonté, ainsi nous appréhendons les différentes situations du quotidien avec plus de recule, nous devenons plus clairevoyant par conséquent nous prenons de meilleurs décisions. Progressivement nous allons changer, devenir plus raisonnable, compatissant, respectueux, tolérant, ouvert d'esprit, notre bien-être va s'affermir.</p>		
+		<br />
+		<p><span class="paragraphe">La</span> méditation de pleine conscience est une médiation laïque aussi elle s’adresse à tous. Si vous êtes croyants elle vous aidera à pratiquer votre religion.</p>
 
 		</div>
 		</div>
